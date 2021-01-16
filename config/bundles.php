d1b4f388d500bff9c8a3f09bafe0096bb2526e05
= <=>
2021-01-16 12:08:05 +0100

    Install eloquent orm

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    WouterJ\EloquentBundle\WouterJEloquentBundle::class => ['all' => true],
 ];
